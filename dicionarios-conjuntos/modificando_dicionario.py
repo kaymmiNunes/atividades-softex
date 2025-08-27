90cdb2fa30277e347dc831a8c743863eb1a18cb8
Kaymmi Nunes <kaymmiknb@gmail.com>
2025-08-27 07:12:51 -0300

    Registro de aluno com nota e aprovação

--- dicionarios-conjuntos/modificando_dicionario.py
+++ dicionarios-conjuntos/modificando_dicionario.py
@@ -1,8 +1,23 @@
+# Registro de aluno com nota e aprovação
+
 aluno = {
     "nome": "Desconhecido",
     "idade": 0, 
-    "nota": 0
+    "nota": 0,
+    "aprovado": False
 }
 
 print("==== Registro do aluno ====")
-print("Nome: ",(aluno["nome"]))
+aluno["nome"] = input("Digite o nome do aluno: ")
+aluno["idade"] = int(input("Digite a idade do aluno: "))
+aluno["nota"] = float(input("Digite a nota do aluno: "))
+
+if aluno["nota"] >= 7:
+    aluno["aprovado"] = True
+else:
+    aluno["aprovado"] = False
+
+print(f'O aluno {aluno["nome"]} de {aluno["idade"]} anos com a nota {aluno["nota"]} está {"aprovado" if  aluno["aprovado"] == True else "reprovado"}')
+
+
+
