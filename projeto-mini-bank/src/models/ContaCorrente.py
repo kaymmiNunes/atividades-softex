59ca518288a831c683da0ef3ca2d8207daba2ada
Kaymmi Nunes <kaymmiknb@gmail.com>
2025-10-05 22:13:50 -0300

    Ambiente

---
+++ projeto-mini-bank/src/models/ContaCorrente.py
@@ -0,0 +1,18 @@
+from models.Conta import Conta
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self._limite = limite
+        self._limite_saques = limite_saques
+
+    def sacar(self, valor):
+        #IMPLEMENTAR
+        return False
+
+    def __str__(self):
+        return f"""\
+            Agência:\t{self.agencia}
+            C/C:\t\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
